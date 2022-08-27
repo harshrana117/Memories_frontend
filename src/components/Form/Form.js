1546c06ffa359b269f49c54bb6899fc2b950cf25
Harsh Rana <harshrana117@gmail.com>
2022-08-28 00:26:04 +0530

    Bug fix api-functions and posts

--- src/components/Form/Form.js
+++ src/components/Form/Form.js
@@ -1,7 +1,101 @@
-import React from 'react'
+import styled from "@emotion/styled";
+import { TextField, Button, Typography, Paper} from "@mui/material";
+import React from "react";
+import FileBase from "react-file-base64";
+import { useDispatch } from "react-redux";
+import { createPost } from "../../actions/posts";
+
+const fileInput = {
+  width: "97%",
+  margin: "10px 0",
+};
+
+const ButtonStyle = {
+       marginBottom: 10,
+   }
+
+const FormStyles = {
+       display: 'flex',
+       flexWrap: 'wrap',
+       justifyContent: 'center',
+       margin: '10px',
+  }
+
+const StyledTextField = styled(TextField)`
+margin: 10px;
+`
 
 export const Form = () => {
+  const dispatch = useDispatch();
+
+  const [postData, setPostData] = React.useState({
+    creator: "",
+    title: "",
+    message: "",
+    selectedFile: "",
+    tags: ''
+  });
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    dispatch(createPost(postData))
+  };
+
+  const Clear = () => {};
   return (
-    <div><h1>Form</h1></div>
-  )
-}
+    <Paper>
+      <form autoComplete="off" noValidate onSubmit={handleSubmit} style={{...FormStyles}}>
+      <Typography variant="h6">Creating a memory</Typography>
+      <StyledTextField
+        name="creator"
+        variant="outlined"
+        label="Creator"
+        fullWidth
+        value={postData.creator}
+        onChange={(e) => setPostData({ ...postData, creator: e.target.value })}
+      />
+
+      <StyledTextField
+        name="title"
+        variant="outlined"
+        label="Title"
+        fullWidth
+        value={postData.title}
+        onChange={(e) => setPostData({ ...postData, title: e.target.value })}
+      />
+
+      <StyledTextField
+        name="message"
+        variant="outlined"
+        label="Message"
+        fullWidth
+        value={postData.message}
+        onChange={(e) => setPostData({ ...postData, message: e.target.value })}
+      />
+
+      <StyledTextField
+        name="tags"
+        variant="outlined"
+        label="Tags"
+        fullWidth
+        value={postData.tags}
+        onChange={(e) => setPostData({ ...postData, tags: e.target.value })}
+      />
+
+      <div style={{ ...fileInput }}>
+        <FileBase
+          type="file"
+          multiple={false}
+          onDone={({ base64 }) =>
+            setPostData({ ...postData, selectedFile: base64 })
+          }
+        ></FileBase>
+      </div>
+
+      <Button style={{...ButtonStyle}} variant='contained' color='primary' size='large' type='submit' fullWidth>Submit</Button>
+      <Button style={{...ButtonStyle}} variant='contained' color='secondary' size='small' type='submit' onClick={Clear} fullWidth>Clear</Button>
+      </form>
+    </Paper>
+  );
+};
--- src/components/Posts/Posts.js
+++ src/components/Posts/Posts.js
@@ -1,17 +1,28 @@
+import { CircularProgress, Grid } from '@mui/material'
 import React from 'react'
 import { useSelector } from 'react-redux'
 import Post from './Post/Post'
 
-const Posts = () => {
+const ContainerStyles = {
+  display: 'flex',
+    alignItems: 'center',
+}
+
+const Posts = ({setCurrentId}) => {
   const posts = useSelector((state) => state.posts)
 
   console.log(posts)
   return (
-    <div>
-      <Post />
-      <h1>Posts</h1>
-    </div>
-  )
+    !posts.length ? <CircularProgress /> : (
+      <Grid style={{...ContainerStyles}} container alignItems="stretch" spacing={3}>
+        {posts.map((post) => (
+          <Grid key={post._id} item xs={12} sm={6} md={6}>
+            <Post post={post} setCurrentId={setCurrentId} />
+          </Grid>
+        ))}
+      </Grid>
+    )
+  );
 }
 
 export default Posts
